45b7cf817a0020d0e630c381b6ead74228d396dc
EMMANUEL EZEOYIRI <emmanuelezeoyirikosi@yahoo.com>
2026-02-13 15:20:23 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 #############################################
-#     Bedrock Project ..
+#     Bedrock Project ....
 #############################################
 
 #############################################
@@ -611,5 +611,6 @@ output "ingress" {
   value = try(kubernetes_ingress_v1.retail_ui.status[0].load_balancer[0].ingress[0].hostname, "ALB not created yet")
 
 }
+
 
 
